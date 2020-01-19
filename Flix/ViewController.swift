e6242ec168a11047f1c03552d415e0b86b8af3cc
Angelo Domingo <domingoangelodominic@gmail.com>
2020-01-19 12:41:23 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Angelo Domingo on 1/19/20.
+//  Copyright © 2020 Angelo Domingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
