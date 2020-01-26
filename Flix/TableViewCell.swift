97e985e54606eb57f23addef85ff7e8959dba94a
Angelo Domingo <domingoangelodominic@gmail.com>
2020-01-26 07:53:41 -0800

    Movie Details Feature Added

---
+++ Flix/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Flix
+//
+//  Created by Angelo Domingo on 1/20/20.
+//  Copyright © 2020 Angelo Domingo. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
