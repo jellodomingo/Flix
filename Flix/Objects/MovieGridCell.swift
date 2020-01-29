5a086e2cbb8e6162dfaf2704da9056216c5eb77f
Angelo Domingo <domingoangelodominic@gmail.com>
2020-01-28 18:18:55 -0800

    Superhero Movie Grid Added

---
+++ Flix/Objects/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  
+//
+//  Created by Angelo Domingo on 1/28/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
